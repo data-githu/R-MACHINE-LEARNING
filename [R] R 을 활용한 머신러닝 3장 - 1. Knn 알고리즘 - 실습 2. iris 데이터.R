256c812c5b66460adbaee6af57b6c859f0e65a8f
data-githu <74716153+data-githu@users.noreply.github.com>
2021-02-09 11:39:00 +0900

    Update [R] R 을 활용한 머신러닝 3장 - 1. Knn 알고리즘 - 실습 2. iris 데이터.R

--- [R] R 을 활용한 머신러닝 3장 - 1. Knn 알고리즘 - 실습 2. iris 데이터.R
+++ [R] R 을 활용한 머신러닝 3장 - 1. Knn 알고리즘 - 실습 2. iris 데이터.R
@@ -1,4 +1,31 @@
 ■ R 을 활용한 머신러닝 3장 - 1. Knn 알고리즘 - 실습 2. iris 데이터
+
+# 데이터 확인
+head( iris )
+nrow(iris)
+unique(iris$Species)
+
+문제187. 아이리스의 종, 아이리스 종별 건수를 출력하시오!
+table(iris$Species)
+
+setosa versicolor  virginica 
+        50         50         50 
+
+문제188. createDataPartition 함수를 이용해서 아이리스 데이터를 훈련 데이터와 테스트 데이터로 나누시오! 훈련 80% 테스트 20%
+iris <-  read.csv("iris(150).csv", header=T,  stringsAsFactors=FALSE)
+library(caret)
+train_num <- createDataPartition(iris$Species, p=0.8, list=F)
+train_data <- iris[train_num,]
+test_data <- iris[-train_num,] # train_num 에 있는 숫자 빼고 다른 숫자
+nrow(train_data) # 120
+nrow(test_data) # 30
+
+table(iris$Species) # 각 50
+
+table(train_data$Species) # 각 40
+table(test_data$Species)  # 각 10
+
+문제189. 위의 훈련데이터와 테스트데이터를 바로 오스트리아 빈대학교에서 만든 knn 함수에 입력하여 테스트 데이터의 라벨을 예측하고 이원교차표를 출력하시오!
 # 1. 데이터 확인
 # head(iris)
 # nrow(iris) 
@@ -14,7 +41,7 @@ iris_n  <- as.data.frame(lapply(iris[2:5],normalize))
 head(iris_n) # 정규화 확인
 
 
-# 3. 테스트 데이터 트레인 데이터 나누기
+# 3. 테스트데이터 트레인 데이터 나누기
 library(caret)
 set.seed(1)
 train_num <- createDataPartition(iris$Species, p=0.8, list=F)
